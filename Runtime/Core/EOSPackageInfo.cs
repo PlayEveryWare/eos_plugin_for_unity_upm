21e5f16e08489a358aef2b7a10d7bc1226898952
arthur272 <arthur740212@gmail.com>
2023-06-15 14:02:03 -0700

    chore : 2.3.2 package version and changelog

--- Runtime/Core/EOSPackageInfo.cs
+++ Runtime/Core/EOSPackageInfo.cs
@@ -37,7 +37,7 @@ public static class EOSPackageInfo
     //VERSION START
     public static string GetPackageVersion()
     {
-        return "2.3.1";
+        return "2.3.2";
     }
     //VERSION END
 }
